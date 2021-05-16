b92879b972aff198f249fc61043377ae5ed9a16b
Satoru Tanaka <ittimfn@gmail.com>
2021-05-16 21:25:06 +0900

    アクセス方法の詳細を追加

--- app/app.py
+++ app/app.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 import settings
 
-print(settings.ENV_DIC)
+keyIndex = 0
+tmp1 = settings.ENV_DIC[settings.ENV_KEYS[keyIndex]]
+keyIndex = keyIndex + 1
+
+tmp2 = settings.ENV_DIC[settings.ENV_KEYS[keyIndex]]
+keyIndex = keyIndex + 1
+
+tmp3 = settings.ENV_DIC[settings.ENV_KEYS[keyIndex]]
+keyIndex = keyIndex + 1
+
+if __name__ == '__main__':
+  print(settings.ENV_DIC)
+  print(tmp1)
+  print(tmp2)
+  print(tmp3)
